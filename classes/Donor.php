7dcffe2cc1214722bf53b9d843b32db611500fce
denver-steiner <denver@steiner7.com>
2023-11-21 23:07:59 -0500

    modify .csv export headers to try to use UTF-8

--- classes/Donor.php
+++ classes/Donor.php
@@ -1037,27 +1037,29 @@ ADD COLUMN `TypeId` INT NULL DEFAULT NULL AFTER `Country`;
         self::donor_update_suggestion($current,$new);
     }
 
-    static function get_email_list(){
-        $SQL="Select D.DonorId, D.Name, D.Name2,`Email`,COUNT(*) as donation_count, SUM(Gross) as Total,DATE(MIN(DT.`Date`)) as FirstDonation, DATE(MAX(DT.`Date`)) as LastDonation
-        FROM ".Donor::get_table_name()." D INNER JOIN ".Donation::get_table_name()." DT ON D.DonorId=DT.DonorId 
-        WHERE D.Email<>'' AND D.EmailStatus=1 AND D.MergedId=0        
-        Group BY D.DonorId, D.Name, D.Name2,`Email` Order BY D.Name";
-        $results = self::db()->get_results($SQL);
-        $fp = fopen(dn_plugin_base_dir()."/resources/email_list.csv", 'w');
-        fputcsv($fp, array_keys((array)$results[0]));//write first line with field names
-        foreach ($results as $r){
-            fputcsv($fp, (array)$r);
-        }
-        fclose($fp);
-    }
+    // static function get_email_list(){
+    //     $SQL="Select D.DonorId, D.Name, D.Name2,`Email`,COUNT(*) as donation_count, SUM(Gross) as Total,DATE(MIN(DT.`Date`)) as FirstDonation, DATE(MAX(DT.`Date`)) as LastDonation
+    //     FROM ".Donor::get_table_name()." D INNER JOIN ".Donation::get_table_name()." DT ON D.DonorId=DT.DonorId 
+    //     WHERE D.Email<>'' AND D.EmailStatus=1 AND D.MergedId=0        
+    //     Group BY D.DonorId, D.Name, D.Name2,`Email` Order BY D.Name";
+    //     $results = self::db()->get_results($SQL);
+    //     $fp = fopen(dn_plugin_base_dir()."/resources/email_list.csv", 'w');
+    //     fputcsv($fp, array_keys((array)$results[0]));//write first line with field names
+    //     foreach ($results as $r){
+    //         fputcsv($fp, (array)$r);
+    //     }
+    //     fclose($fp);
+    // }
 
     static function get_mail_list($where=[]){
         $SQL="Select D.DonorId, D.Name, D.Name2, D.Email,`Address1`, `Address2`, `City`, `Region`, `PostalCode`, `Country`,COUNT(*) as donation_count, SUM(Gross) as Total,DATE(MIN(DT.`Date`)) as FirstDonation, DATE(MAX(DT.`Date`)) as LastDonation
         FROM ".Donor::get_table_name()." D INNER JOIN ".Donation::get_table_name()." DT ON D.DonorId=DT.DonorId 
         WHERE ".(sizeof($where)>0?implode(" AND ",$where):" 1 ")."    
         Group BY D.DonorId, D.Name, D.Name2,`Address1`, `Address2`, `City`, `Region`, `PostalCode`, `Country` Order BY D.Name";   
-        $results = self::db()->get_results($SQL);        
-        header('Content-Type: text/csv; charset=utf-8');
+        $results = self::db()->get_results($SQL);         
+        header("Content-Transfer-Encoding: UTF-8");
+        header('Content-type: application/csv');
+        header('Pragma: no-cache');
         header('Content-Disposition: attachment; filename=Donors.csv');
         $fp = fopen('php://memory', 'r+');
         fputcsv($fp, array_keys((array)$results[0]));//write first line with field names
