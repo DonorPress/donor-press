6a04b578c9a57ec62eef6d5d28f6b968041cfed6
denver-steiner <denver@steiner7.com>
2024-01-13 13:05:33 -0500

    switch to php version compare

--- donor-upgrade.php
+++ donor-upgrade.php
@@ -3,30 +3,31 @@ function donor_press_upgrade(){
 	global $donor_press_db_version;
 	$current_db_version=get_option( "donor_press_db_version");
 
-	if (!$current_db_version || donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.3')){
+	if (!$current_db_version || version_compare($current_db_version, '0.0.3', '<')){
 		donor_press_upgrade_003();
 	}
-
-	if (donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.4')){ 
+	
+	if (version_compare($current_db_version, '0.0.4', '<')){ 
 		donor_press_upgrade_004();
 	}
-	if (donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.5')){ 
+	
+	if (version_compare($current_db_version, '0.0.5', '<')){ 
 		donor_press_upgrade_005();
 	}
-
-	if (donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.6')){ 
+	
+	if (version_compare($current_db_version, '0.0.6', '<')){ 
 		donor_press_upgrade_006();
 	}
-
-	if (donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.7')){ 
+	
+	if (version_compare($current_db_version, '0.0.7', '<')){ 
 		donor_press_upgrade_007();
 	}
-
-	if (donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.8')){ 
+	
+	if (version_compare($current_db_version, '0.0.8', '<')){ 
 		donor_press_upgrade_008();
 	}
-
-	if (donor_upgrade_version_value($current_db_version)<donor_upgrade_version_value('0.0.9')){ 
+	
+	if (version_compare($current_db_version, '0.0.9', '<')){ 
 		donor_press_upgrade_009();
 	}
 
@@ -36,17 +37,6 @@ function donor_press_upgrade(){
 		add_option( "donor_press_db_version", $donor_press_db_version );
 	}
 	Donor::display_notice("Donor Press Database Upgraded from ".$current_db_version." to ".$donor_press_db_version);
-}
-
-//function version_check($current_db_version,)
-function donor_upgrade_version_value($version){
-	//example: 1.2.3 = 1*100^2+2*100^1+3*100^0=10203  1.2.30 = 10230. This assumes a max max of two digts. 99.99.99=999999
-	$segment=explode(".",$version);
-	$sum=0;
-	for($i=0;$i<sizeof($segment);$i++){
-		$sum+=$segment[$i]*pow(100,sizeof($segment)-$i-1);
-	}
-	return $sum;
 }
 
 function donor_press_upgrade_003(){
